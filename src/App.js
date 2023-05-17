c8c7750d6e2e72695227a2f14fbc42fe5e0e18a1
YuvarajNagi <yuvvrajnagi@gmail.com>
2023-05-17 01:02:26 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TodoList } from './components/ToDoList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
+
